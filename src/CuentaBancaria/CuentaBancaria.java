4800adc28be81a77e0fac995ae8e0af0ef2ac081
Jordán Shokida <jordii.s@hotmail.com>
2022-09-08 14:21:45 -0300

    Agregué métodos para sumar, restar y obtener el tipo de cuenta. Y completé el metodo main

--- src/CuentaBancaria/CuentaBancaria.java
+++ src/CuentaBancaria/CuentaBancaria.java
@@ -1,5 +1,7 @@
 package CuentaBancaria;
 
+import java.util.Scanner;
+
 /*Crear una clase Cuenta bancaria! La misma tiene que tener los siguientes atributos/estados: estaActiva, cantidadDeFondos, nombreTitular.
  Y aparte de los getter y setter de estos atributos, un metodo que nos devuelva un String que nos diga que categoria es la cuenta, categoria “A” es que tiene mas de 100.000,
   categoria “B” es que tiene entre 50.000 a 99.999, categoria “C” que tiene entre 1 a 49.999, y categoria “D” sino tiene fondos! Y otros 2 metodos que me permitan sumar o
@@ -16,6 +18,7 @@ public class CuentaBancaria {
         this.cantidadDeFondos = cantidadDeFondos;
         this.nombreTitular = nombreTitular;
     }
+
     //getters y setters
     public boolean isEstaActiva() {
         return estaActiva;
@@ -41,10 +44,51 @@ public class CuentaBancaria {
         this.nombreTitular = nombreTitular;
     }
 
-    public void SumarFondos(){
+    public void setTipoDeCuenta(double cantidadDeFondos) {
+        if (cantidadDeFondos >= 100000) {
+            System.out.println("\n Su categoria es la A");
+        } else if (cantidadDeFondos < 100000 && cantidadDeFondos >= 50000) {
+            System.out.println("\n Su categoria es la B");
+        } else if (cantidadDeFondos < 50000 && cantidadDeFondos > 0) {
+            System.out.println("\n Su categoria es la C");
+        } else if (cantidadDeFondos == 0) {
+            System.out.println("\n Su categoria es la D");
+        } else {
+            System.out.println("Error, su cuenta no puede ser negativa");
+            System.exit(-1);
+        }
+
 
     }
 
-    public void restarFondos(){
+    Scanner sc = new Scanner(System.in);
+
+    public double SumarFondos(double cantidadDeFondos) {
+        System.out.println("Indique el monto a ingresar");
+        double ingresar = sc.nextDouble();
+        if (ingresar >= 0) {
+            return this.cantidadDeFondos = cantidadDeFondos + ingresar;
+        } else {
+            System.out.println("No se puede ingresar un número negativo");
+            return cantidadDeFondos;
+        }
+
+
+    }
+
+    public double restarFondos(double cantidadDeFondos) {
+        System.out.println("ingrese la cantidad a retirar");
+        double ingresar = sc.nextDouble();
+        if ((cantidadDeFondos - ingresar) < 0) {
+            System.out.println("No puede retirar más de " + cantidadDeFondos + " pesos ");
+            return cantidadDeFondos;
+        }
+        if (ingresar < 0) {
+            System.out.println("No se puede ingresar montos negativos");
+            return cantidadDeFondos;
+        } else {
+            return this.cantidadDeFondos = cantidadDeFondos - ingresar;
+        }
+
     }
 }
--- src/CuentaBancaria/UsoCuentas.java
+++ src/CuentaBancaria/UsoCuentas.java
@@ -1,7 +1,33 @@
 package CuentaBancaria;
 
+import java.util.Scanner;
+
 public class UsoCuentas {
     public static void main(String[] args) {
+        CuentaBancaria cuenta = new CuentaBancaria(true, 20000, "Jordán Shokida");
+        cuenta.setTipoDeCuenta(cuenta.getCantidadDeFondos());
+
+        System.out.println("Bienvenido a su cuenta, señor: " + cuenta.getNombreTitular()  + "\nSu saldo actual es de " + cuenta.getCantidadDeFondos() +  "\nPara ingresar dinero ingrese 1 \nPara retirar dinero ingrese 2 \nPara salir ingrese 0");
+
+        Scanner input = new Scanner(System.in);
+        int opcion = input.nextInt();
+
+        if (opcion == 1) {
+            cuenta.SumarFondos(cuenta.getCantidadDeFondos());
+            cuenta.setTipoDeCuenta(cuenta.getCantidadDeFondos());
+            System.out.println("Total: " + cuenta.getCantidadDeFondos());
+        } else if (opcion == 2) {
+            cuenta.restarFondos(cuenta.getCantidadDeFondos());
+            cuenta.setTipoDeCuenta(cuenta.getCantidadDeFondos());
+            System.out.println("Total: " + cuenta.getCantidadDeFondos());
+        } else if (opcion == 0) {
+
+            System.out.println("Operación terminada ");
+            System.exit(0);
+        } else {
+            System.out.println("Error, debe ingresar la opción requerida.");
+        }
+
 
     }
 }
